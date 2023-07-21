624623273061e8ec7a4d32f9521d6d62f3a74baa
duythai2108 <thaiduy21082000@gmail.com>
2023-07-21 09:39:25 +0700

    adding code spliting + lazy load

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "swiper/scss";
 import Main from "./components/layout/Main";
-import HomePage from "./pages/HomePage";
 import Banner from "./components/banner/Banner";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+// import HomePage from "./pages/HomePage";
+// import MoviePage from "./pages/MoviePage";
+// import MovieDetailsPage from "./pages/MovieDetailsPage";
+
+// lazy load 
+// * dynamic import
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner></Banner>
-                <HomePage></HomePage>
-              </>
-            }
-          ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movie/:movieId"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
